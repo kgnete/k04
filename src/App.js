d67607bc24877656b6182fe8a3166f0428781273
Your Name <you@example.com>
2018-12-05 11:30:45 +0100

    k04

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="http://turfjs.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn turf
           </a>
         </header>
       <Map/>
