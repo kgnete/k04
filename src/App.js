5aefa7cd8ac26c07b25a05584ac93fee941c00f2
Your Name <you@example.com>
2018-12-05 12:26:33 +0100

    k04

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import pp from './pp.jsx'
-
-
+import Pp from './pp.jsx'
 
 import ReactEcharts from 'echarts-for-react';
-import { greatCircle, point } from '@turf/turf';
+import {greatCircle, point} from '@turf/turf';
 import Map from './map.jsx'
-
-
-console.log(greatCircle([0, 0], [100, 10]));
+console.log(greatCircle([
+  0, 0
+], [100, 10]));
 console.log(point([100, 0]));
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>turf
-          </p>
+        <header>
           <a
             className="App-link"
             href="http://turfjs.org/"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn turf
           </a>
         </header>
-      <Map/>
+        <Map/>
+        <Pp/>
       </div>
     );
   }
 }
 
 export default App;
-
-
--- src/pp.jsx
+++ src/pp.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import turf from '@turf/turf';
 
 export default class pp extends Component {
   render() {
+var line = turf.lineString([[-83, 30], [-84, 36], [-78, 41]]);
+var options = {units: 'miles'};
+var along = turf.along(line, 200, options);
+
     return (
       <div>
         <h1>hola caracola</h1>
