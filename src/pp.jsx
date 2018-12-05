9945f6743e31564d910b7dab816eb7fba51c68c6
Your Name <you@example.com>
2018-12-05 12:01:49 +0100

    k04

---
+++ src/pp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class pp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hola caracola</h1>
+      </div>
+    )
+  }
+}
